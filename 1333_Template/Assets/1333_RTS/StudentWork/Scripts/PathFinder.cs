5efbb79ab5e8b1c537d9f9e3307bfc772eb140e6
Epicjeong <yurijeong@icloud.com>
2026-06-10 11:48:23 -0700

    thatsalotta vars

--- 1333_Template/Assets/1333_RTS/StudentWork/Scripts/PathFinder.cs
+++ 1333_Template/Assets/1333_RTS/StudentWork/Scripts/PathFinder.cs
@@ -32,10 +32,10 @@ public class PathFinder : MonoBehaviour
     [SerializeField] private Color _pathColor = Color.yellow;
     [SerializeField] private Color _visitedColor = new Color(.5f, .5f, 1, .5f);
     [SerializeField] private Color _unvisitedColor = new Color(.3f, .3f, .3f, .3f);
-    [SerializeField] private Color _finalPath = Color.cyan;
+    [SerializeField] private Color _finalPathColor = Color.cyan;
     [SerializeField] private Color _currentNodeColor = Color.magenta;
-    [SerializeField] private Color _neighbor = new Color(1f, .5f, 0, .5f);
-    [SerializeField] private Color _exploreLine = new Color(1f, 1f, 0, .3f);
+    [SerializeField] private Color _neighborColor = new Color(1f, .5f, 0, .5f);
+    [SerializeField] private Color _exploreLineColor = new Color(1f, 1f, 0, .3f);
 
     [Header("Visual Settings")]
     [SerializeField] private int _currentSeed = 0;
@@ -52,5 +52,18 @@ public class PathFinder : MonoBehaviour
     private System.Random _seed;
 
     //Visualize state
-    private HashSet
+    private HashSet<Vector2Int> _visitedNodes = new HashSet<Vector2Int>();
+    private Dictionary<Vector2Int, int> _nodeDistance = new Dictionary<Vector2Int, int>(); // distance from start
+    private List<Vector2Int> _currentPath = new List<Vector2Int>();
+    private List<Vector2Int> _reconstructPath = new List<Vector2Int>();
+    private Vector2Int? _startNode;
+    private Vector2Int? _endNode;
+    private VisualizationState _visualization = VisualizationState.Idle;
+    private Coroutine _currentVisual;
+    private List<Vector3> _finalPath;
+    private bool _shouldPause;
+    private bool _isStepMode;
+    private List<Vector2Int> _exploreOrder;
+    private Vector2Int? _currentNode;
+    private List<Vector2Int> _currentNeighbors;
 }
